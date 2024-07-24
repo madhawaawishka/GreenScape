15c080cc7794bc9b5128daff29f377124e620ba3
Madhawa Awishka <162104267+madhawaawishka99@users.noreply.github.com>
2024-07-25 00:00:37 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider =  (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000"
+  const url="https://greenscape-backend.onrender.com"
   const [token,setToken]=useState("");
   const [food_list,setFoodList]=useState([])
 
